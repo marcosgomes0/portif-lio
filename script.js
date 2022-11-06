85fc98b854e0ac015aba9b6edb3be69b084ab5b5
Marcos Gomes <marcosmuriloribeirogomes@gmail.com>
2022-11-06 20:28:54 -0300

    animação e ativação do mobile-menu

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const linksMenu = document.querySelectorAll(".header-list a");
 const mobileMenu = document.querySelector(".menu-mobile");
+const linhas = document.querySelectorAll(".menu-mobile > div");
 const mobileLista = document.querySelector(".navega");
+
+
+
+
 
 function toggleMenu() {
   mobileLista.classList.toggle("ativo");
+  linhas.forEach(linha=>{
+    linha.classList.toggle('ativo')
+  })
 }
 
-mobileMenu.addEventListener("click", toggleMenu);
+["click", "touchstart"].forEach((eventos) => {
+  mobileMenu.addEventListener(eventos, toggleMenu);
+});
 
-function linkClick() {
+function linkClick(e) {
+  e.preventDefault()
   mobileLista.classList.remove("ativo");
 }
 
