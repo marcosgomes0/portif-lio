852d5bb653788ce5027544350c6c93e4004f88fd
Marcos Gomes <marcosmuriloribeirogomes@gmail.com>
2022-11-07 16:07:48 -0300

    finalização do slide

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ function initBurgerMenu() {
   function linkClick(e) {
     e.preventDefault();
     mobileLista.classList.remove("ativo");
+    linhas.forEach((linha) => {
+      linha.classList.remove("ativo");
+    });
   }
   // Remoção do menu após o click em algum link
   linksMenu.forEach((link) => {
@@ -28,12 +31,75 @@ initBurgerMenu();
 const slide = document.querySelector(".slide");
 const slideLista = document.querySelector(".slide-list");
 
+const arrayImg = [...slideLista.children].map((element) => {
+  const position = slidePosition(element);
+  return {
+    position,
+    element,
+  };
+});
+
 const dist = {
   finalPosition: 0,
   startX: 0,
   moviPosition: 0,
   moviment: 0,
 };
+
+const slidePositions = {
+  prev: 0,
+  actual: 0,
+  next: 0,
+};
+
+function slidePosition(element) {
+  const margin = (slide.offsetWidth - element.offsetWidth) / 2;
+  return -(element.offsetLeft - margin);
+}
+
+function slideIndex(index) {
+  const last = arrayImg.length - 1;
+  slidePositions.prev = index ? index - 1 : undefined;
+  slidePositions.actual = index;
+  slidePositions.next = index == last ? undefined : index + 1;
+}
+
+function slideTransition(active){
+  slideLista.style.transition = active ? "transform 0.3s" : ''
+}
+
+function changeSlide(index) {
+  const activeSlide = arrayImg[index];
+  slideIndex(index);
+  moveSlide(activeSlide.position);
+  dist.finalPosition = activeSlide.position;
+}
+changeSlide(1);
+
+function nextSlide() {
+  if (slidePositions.next != undefined) {
+    changeSlide(slidePositions.next);
+  }
+}
+
+function prevSlide() {
+  if (slidePositions.prev != undefined) {
+    changeSlide(slidePositions.prev);
+  }
+}
+
+function autoChangeSlide(){
+  if (dist.moviment > 120 && slidePositions.next != undefined){
+    nextSlide()
+  }
+  else if (dist.moviment < -120 && slidePositions.prev != undefined){
+    prevSlide()
+  } 
+  else {
+    changeSlide(slidePositions.actual)
+  }
+}
+
 
 function moveSlide(pos) {
   dist.moviPosition = pos;
@@ -42,6 +108,7 @@ function moveSlide(pos) {
 
 function updatePosition(clientX) {
   const movimento = (dist.startX - clientX) * 1.5;
+  dist.moviment = movimento
   return dist.finalPosition - movimento;
 }
 
@@ -50,10 +117,10 @@ function onMove(e) {
     e.type == "mousemove" ? e.clientX : e.changedTouches[0].clientX;
   const posição = updatePosition(pointerPosition);
   moveSlide(posição);
-  console.log(posição);
 }
 
 function onStart(e) {
+  slideTransition(false)
   const movetype = e.type == "mousedown" ? "mousemove" : "touchmove";
   if (e.type == "mousedown") {
     e.preventDefault();
@@ -69,6 +136,8 @@ function onEnd(e) {
   const movetype = e.type == "mouseup" ? "mousemove" : "touchmove";
   slide.removeEventListener(movetype, onMove);
   dist.finalPosition = dist.moviPosition;
+  slideTransition(true)
+  autoChangeSlide()
 }
 
 slide.addEventListener("mousedown", onStart);
